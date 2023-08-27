d2044e91c913825b4a9c73fc230d698317a29185
Camilo Faria <camilozabaleta65@gmail.com>
2023-08-27 18:44:20 -0500

    Config deploy Github Pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: "/heuristic-evaluation-app/",
     plugins: [react()],
-    esbuild: {
-        jsxFactory: "h",
-        jsxFragment: "Fragment",
-        // Agregue esta línea
-        format: "esm",
-    },
 });
